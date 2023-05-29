5d3dd1f6b07b30aa4e03054a2be0bafe1c76a88f
chain <klaus.mika1052@gmail.com>
2023-05-29 16:42:49 -0300

    Produto & Checkout - Caregando dados do produto

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,19 +1,52 @@
+import { stripe } from '@/lib/stripe'
 import Image from 'next/image'
+import Stripe from 'stripe'
 
-export default function Product() {
+export async function generateStaticParams() {
+  const products = await stripe.products.list()
+
+  const productIds = products.data.map((product) => ({ id: product.id }))
+
+  return productIds
+}
+
+export default async function Product({ params }: { params: { id: string } }) {
+  const productId = params.id
+  const response = await stripe.products.retrieve(productId, {
+    expand: ['default_price'],
+  })
+
+  const price = response.default_price as Stripe.Price
+
+  const product = {
+    id: response.id,
+    name: response.name,
+    imageUrl: response.images[0],
+    price: new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    }).format((price.unit_amount as number) / 100),
+    description: response.description,
+  }
+
   return (
     <main className="mx-auto grid  max-w-[1180px] grid-cols-2 items-center gap-16 ">
       <div className="flex h-[556px] w-full max-w-[576px] grid-flow-col  items-center justify-center rounded-lg bg-tshirt p-1 ">
-        <Image src={''} alt="" className="object-cover" />
+        <Image
+          src={product.imageUrl}
+          width={520}
+          height={480}
+          alt=""
+          className="object-cover"
+        />
       </div>
       <div className="flex h-full flex-col pt-5">
-        <h1 className="text-3xl text-gray-300">Camiseta 1</h1>
-        <span className="mt-4 block text-3xl text-green-300">R$ 49,90</span>
+        <h1 className="text-3xl text-gray-300">{product.name}</h1>
+        <span className="mt-4 block text-3xl text-green-300">
+          {product.price}
+        </span>
         <p className="mt-10 text-lg leading-relaxed text-gray-300">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
-          impedit, laborum explicabo quas ea quam reiciendis voluptas
-          voluptatibus ipsam numquam eveniet vero repudiandae vitae praesentium
-          in, iure est quasi quisquam?
+          {product.description}
         </p>
         <button className="mt-auto cursor-pointer rounded-lg border-0 bg-green-500 p-5 text-lg font-bold text-white hover:bg-green-300">
           Compre agora
