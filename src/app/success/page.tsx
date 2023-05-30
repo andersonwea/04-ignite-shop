9a6869fcec8538e72f13761ab0c544c6fd96d803
chain <klaus.mika1052@gmail.com>
2023-05-29 22:21:44 -0300

    Produto & Checkout - Estrutura de sucesso

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,3 +1,25 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
 export default function Success() {
-  return <h1>success</h1>
+  return (
+    <main className="mx-auto mt-5 flex h-[656px] flex-col items-center">
+      <h1 className="text-3xl text-gray-100">Compra efetuada!</h1>
+
+      <div className="item-center mt-16 flex h-[145px] w-full max-w-[130px] justify-center rounded-lg bg-tshirt p-1">
+        <Image src={''} alt="" className="object-cover" />
+      </div>
+      <p className="mt-8 max-w-xl text-center text-2xl text-gray-300">
+        Uhuul <strong>Diego Fernandes</strong>, sua{' '}
+        <strong>Camiseta Beyond the Limits</strong> já está a caminho da sua
+        casa.{' '}
+      </p>
+      <Link
+        className="mt-20 block text-xl font-bold text-green-500 hover:text-green-300"
+        href={'/'}
+      >
+        Voltar ao catálogo
+      </Link>
+    </main>
+  )
 }
