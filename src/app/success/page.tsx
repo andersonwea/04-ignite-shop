73f9fc6d76333a01f5f1cb3d0d2b5a25a4434d40
chain <klaus.mika1052@gmail.com>
2023-05-29 23:32:12 -0300

    Produto & Checkout - Dados da compra no sucesso]

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,18 +1,44 @@
+import { stripe } from '@/lib/stripe'
 import Image from 'next/image'
 import Link from 'next/link'
+import Stripe from 'stripe'
 
-export default function Success() {
+export default async function Success({
+  searchParams,
+}: {
+  searchParams: { session_id: string }
+}) {
+  const sessionId = searchParams.session_id
+
+  const session = await stripe.checkout.sessions.retrieve(sessionId, {
+    expand: ['line_items', 'line_items.data.price.product'],
+  })
+
+  const customerName = session.customer_details?.name
+  const productReponse = session.line_items?.data[0].price
+    ?.product as Stripe.Product
+
+  const product = {
+    name: productReponse.name,
+    imageUrl: productReponse.images[0],
+  }
+
   return (
     <main className="mx-auto mt-5 flex h-[656px] flex-col items-center">
       <h1 className="text-3xl text-gray-100">Compra efetuada!</h1>
 
       <div className="item-center mt-16 flex h-[145px] w-full max-w-[130px] justify-center rounded-lg bg-tshirt p-1">
-        <Image src={''} alt="" className="object-cover" />
+        <Image
+          src={product.imageUrl}
+          alt=""
+          className="object-cover"
+          width={120}
+          height={110}
+        />
       </div>
       <p className="mt-8 max-w-xl text-center text-2xl text-gray-300">
-        Uhuul <strong>Diego Fernandes</strong>, sua{' '}
-        <strong>Camiseta Beyond the Limits</strong> já está a caminho da sua
-        casa.{' '}
+        Uhuul <strong>{customerName}</strong>, sua{' '}
+        <strong>{product.name}</strong> já está a caminho da sua casa.{' '}
       </p>
       <Link
         className="mt-20 block text-xl font-bold text-green-500 hover:text-green-300"
